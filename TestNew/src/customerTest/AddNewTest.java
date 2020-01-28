147508fb086ab92c1f6fc5ee9342d4665021f435
sriharsha <sriharsha@sriharsha-PC>
2020-01-28 22:56:17 +0530

    its new commit

---
+++ TestNew/src/customerTest/AddNewTest.java
@@ -0,0 +1,12 @@
+package customerTest;
+
+public class AddNewTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("hey,its AddNewTest");
+	}
+
+}
